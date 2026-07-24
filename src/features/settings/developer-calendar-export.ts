dcf7c8e6a2cdc2795ab0632006b07de44488f5ca
Max Reuter <max.reuter@pm.me>
2026-07-24 20:51:59 +0300

    refactor: use production calendar fixture dates

--- src/features/settings/developer-calendar-export.ts
+++ src/features/settings/developer-calendar-export.ts
@@ -1,16 +1,16 @@
-import { localDate } from '../../../tests/local-date'
+import { toLocalDateString } from '../../lib/date'
 import { exportBookingCalendar } from '../calendar/booking-calendar-export'
 
 const DEVELOPER_CALENDAR_SELECTIONS = [
   {
     cableId: 'pro',
-    date: localDate('2026-05-20'),
+    date: toLocalDateString('2026-05-20'),
     endTime: '16:00',
     startTime: '15:00',
   },
   {
     cableId: 'easy',
-    date: localDate('2026-05-21'),
+    date: toLocalDateString('2026-05-21'),
     endTime: '11:00',
     startTime: '10:00',
   },
